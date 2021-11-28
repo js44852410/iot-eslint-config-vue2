7469a664b28cd401ed72b604624c724ff3ab0e34
liulipeng <liulipeng@longfor.com>
2021-11-28 14:44:30 +0800

    update

--- IoT.js
+++ IoT.js
@@ -11,6 +11,23 @@ module.exports = {
     './rules/vue',
     './rules/iot-config'
   ].map(require.resolve),
+   env: { // 环境配置
+    es6: true, // 启用ES6的功能
+    node: true, // Node.js全局变量和Node.js范围。
+    browser: true, // 启用浏览器全局变量。
+  },
+  parserOptions: { // 解析器选项
+    ecmaVersion: 6, // 指定你想要使用的 ECMAScript 版本
+    sourceType: 'module', // 设置为 "script" (默认) 或 "module"（如果你的代码是 ECMAScript 模块)
+    parser: 'babel-eslint', // 解析器，默认使用Espree
+    ecmaFeatures: { // 指定要使用其他那些语言对象
+      generators: false,
+      objectLiteralDuplicateProperties: false,
+    },
+  },
+  plugins: [ // 插件
+    'import',
+  ],
   rules: {},
   overrides: [
     {
