5bcae60869eb15bfe450517674f4308056f08d81
liulipeng <liulipeng@longfor.com>
2021-11-28 13:52:30 +0800

    update

--- IoT.js
+++ IoT.js
@@ -11,9 +11,14 @@ module.exports = {
     './rules/vue',
     './rules/iot-config'
   ].map(require.resolve),
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-  }
+  rules: {},
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off',
+        'vue/script-indent': ['error', 2, { baseIndent: 1 }]
+      }
+    }
+  ]
 };
